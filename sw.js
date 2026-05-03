97eac439b5b8c10bf3f80c85b4660ff81fb33594
dhx71 <donald.hamel@gmail.com>
2026-05-03 07:53:34 -0400

    now should be available offline

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE = 'rv-level-v1';
+const FILES = [
+  './',
+  './index.html',
+  './top-view.jpeg',
+  './icon.jpeg',
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
